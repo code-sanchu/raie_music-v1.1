e1ab87b13d02905fe087fc92ef918dc3953a1fc9
code-sanchu <a.ruben00001@gmail.com>
2023-10-10 14:45:37 +0100

    about design polish, img text, responsive

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,15 +1,17 @@
 <script lang="ts" context="module">
 	import { images } from '$lib/assets';
-	import { LinkIconSwitch, PageLayout, Picture, Tooltip } from '$lib/components';
+	import { Icon, LinkIconSwitch, PageLayout, Picture, Tooltip } from '$lib/components';
+	import Bar from '$lib/components/music-player/animated-bars/bar.svelte';
 	import { Link } from '$lib/components/prose';
 	import { contacts, raie_links_arrs } from '$lib/data';
+	import { Envelope, Phone } from 'phosphor-svelte';
 </script>
 
 <PageLayout.VerticalSpacing sizing="1.5" />
 
-<div class="relative prose max-w-[880px] leading-relaxed text-lg sm:text-xl tracking-wide">
+<div class="relative prose text-my-black max-w-[880px] leading-relaxed text-lg tracking-wider">
 	<p>
-		<span class="uppercase tracking-widest text-xs italic mr-lg">About Raie</span>
+		<span class="uppercase tracking-widest text-xs italic mr-lg text-my-black-400">About Raie</span>
 		Raie has performed in a host of top venues in London including Hammersmith Apollo, The Forge Camden,
 		Camden Round House, The Albany Theatre in Deptford as well as more recently at Purcell Room on London’s
 		Southbank.<br /><br />
@@ -29,29 +31,41 @@
 <PageLayout.VerticalSpacing sizing="3/4" />
 
 <div
-	class="flex flex-col-reverse sm:flex-row gap-lg gap-y-[4.5rem] justify-between tracking-widest">
+	class="flex flex-col-reverse sm:flex-row flex-wrap-reverse gap-lg gap-y-[4.5rem] justify-between">
 	<div class="flex flex-col gap-xl">
 		<div>
-			<h2 class="uppercase italic">contacts</h2>
+			<h2 class="uppercase italic tracking-widest text-my-black-600 text-sm">contacts</h2>
 
-			<div class="mt-xs">
-				<a class="block font-medium" href={`mailto:${contacts.email}`} target="_blank"
-					>{contacts.email}</a>
-				<p class="font-medium text-sm mt-xs">{contacts.phone}</p>
+			<div class="mt-md flex flex-col gap-x-md items-end gap-y-md">
+				<div class="flex items-center gap-xs">
+					<span class="text-my-black-300">
+						<Envelope weight="thin" />
+					</span>
+					<a class="tracking-wider" href={`mailto:${contacts.email}`} target="_blank"
+						>{contacts.email}</a>
+				</div>
+
+				<!-- <span class="text-my-black-100 self-center text-xs"> | </span> -->
+				<div class="flex items-center gap-xs">
+					<span class="text-my-black-300">
+						<Phone weight="thin" />
+					</span>
+					<a class="text-sm tracking-wider" href={`tel:${contacts.phone}`}>{contacts.phone}</a>
+				</div>
 			</div>
 		</div>
 
 		<div>
-			<h2 class="uppercase italic">social media</h2>
+			<h2 class="uppercase italic tracking-widest text-sm">social media</h2>
 
-			<div class="flex text-xl items-center gap-sm mt-xs">
+			<div class="flex text-lg items-center gap-sm mt-sm">
 				{#each [...raie_links_arrs.follow] as raieLink}
 					<a
 						class="rounded-full p-xxs"
 						href={raieLink.href}
 						target="_blank"
 						id={`about-${raieLink.id}`}>
-						<LinkIconSwitch type={raieLink.id} weight="fill" />
+						<LinkIconSwitch type={raieLink.id} weight="thin" />
 					</a>
 					<Tooltip text={raieLink.name} triggeredById={`about-${raieLink.id}`} />
 				{/each}
@@ -59,16 +73,16 @@
 		</div>
 
 		<div>
-			<h2 class="uppercase italic">Listen & Buy</h2>
+			<h2 class="uppercase italic tracking-widest text-sm">Listen & Buy</h2>
 
-			<div class="flex text-xl items-center gap-sm mt-xs">
+			<div class="flex text-lg items-center gap-sm mt-sm">
 				{#each [...raie_links_arrs.listen, ...raie_links_arrs.buy] as raieLink}
 					<a
 						class="rounded-full p-xxs"
 						href={raieLink.href}
 						target="_blank"
 						id={`about-${raieLink.id}`}>
-						<LinkIconSwitch type={raieLink.id} weight="fill" />
+						<LinkIconSwitch type={raieLink.id} weight="thin" />
 					</a>
 					<Tooltip text={raieLink.name} triggeredById={`about-${raieLink.id}`} />
 				{/each}
@@ -76,31 +90,49 @@
 		</div>
 	</div>
 
-	<div class="flex gap-lg justify-end">
-		<Picture
-			imageClass="w-[200px] rounded-sm"
-			pictureClass="grayscale"
-			meta={images.raie_as_child.src}
-			sizes={'200px'}
-			loading="eager"
-			alt="" />
+	<div class="flex gap-lg justify-end flex-grow">
+		<div class="w-[200px]">
+			<Picture
+				imageClass="w-full rounded-sm"
+				pictureClass="grayscale"
+				meta={images.raie_as_child.src}
+				sizes={'200px'}
+				loading="eager"
+				alt="" />
+			<p
+				class="text-sm mt-xxs tracking-wide text-my-black-700 underline decoration-my-black-400 underline-offset-1">
+				Raie as a young performer – singing Joan Baez’ Ghetto
+			</p>
+		</div>
 
 		<div class="flex flex-col items-end gap-lg">
-			<Picture
-				imageClass="w-[250px] rounded-sm"
-				pictureClass="grayscale"
-				meta={images.galaxy['full-band'].src}
-				sizes={'200px'}
-				loading="eager"
-				alt="" />
+			<div class="w-[250px]">
+				<Picture
+					imageClass="w-full rounded-sm"
+					pictureClass="grayscale"
+					meta={images.galaxy['full-band'].src}
+					sizes={'200px'}
+					loading="eager"
+					alt="" />
+				<p
+					class="text-sm mt-xxs tracking-wide text-my-black-700 underline decoration-my-black-400 underline-offset-1">
+					The band ends six beautiful days at Galaxy
+				</p>
+			</div>
 
-			<Picture
-				imageClass="w-[250px] rounded-sm"
-				pictureClass="grayscale"
-				meta={images.galaxy['raie-lead_vocal-young_love'].src}
-				sizes={'200px'}
-				loading="eager"
-				alt="" />
+			<div class="w-[250px]">
+				<Picture
+					imageClass="w-full rounded-sm"
+					pictureClass="grayscale"
+					meta={images.galaxy['raie-lead_vocal-young_love'].src}
+					sizes={'200px'}
+					loading="eager"
+					alt="" />
+				<p
+					class="text-sm mt-xxs tracking-wide text-my-black-700 underline decoration-my-black-400 underline-offset-1">
+					Raie at the mic in Galaxy Studios
+				</p>
+			</div>
 		</div>
 	</div>
 </div>
